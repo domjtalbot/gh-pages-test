1faa65daf48bca2701a896610105d8b650cbfe07
Dom Talbot <3301714+domjtalbot@users.noreply.github.com>
2024-08-23 11:26:14 +0000

    fix docs url

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://domjtalbot.github.io/ghpage-test/',
+  url: 'https://domjtalbot.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ghpage-test/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
